ebdc7566934cbb4c4e6e2a9fbf6907111b1c6eba
Haritha944 <harithams23@gmail.com>
2024-11-05 18:02:20 +0530

    signup backend

--- account/views.py
+++ account/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RegisterSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response({
+                "message": "User registered successfully!"
+            }, status=status.HTTP_201_CREATED)
+        return Response({"user": {"email": user.email, "phone_number": user.phone_number}}, status=status.HTTP_201_CREATED)
